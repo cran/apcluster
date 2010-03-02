0e83a16d22623cda7d2bf756fffa4762a6ffc05a
Ulrich Bodenhofer <apcluster@bioinf.jku.at>
2010-03-02 16:57:42 +0000

    version 1.0.1

---
+++ R/apclusterK.R
@@ -0,0 +1,110 @@
+apclusterK <- function(s, K, prc=10, bimaxit=20, exact=FALSE,
+                       nonoise=FALSE, seed=NA, ...)
+{
+    if (!is.na(seed)) set.seed(seed)
+
+    #
+    # check input data
+    #
+
+    if (length(dim(s)) != 2 || ncol(s) != nrow(s))
+    {
+        stop("s must be a square matrix")
+    }
+
+    N <- nrow(s)
+
+    if (K < 2 || K >= N)
+    {
+        stop("Number of data samples is ", N, ".\n",
+             "       Meaningful range for K: 2 to ", N - 1, "\n", call.=FALSE)
+    }
+
+    pminmax <- preferenceRange(s, exact)
+
+    lopref <- pminmax[1]
+    hipref <- pminmax[2]
+    lok    <- 1
+    hik    <- N
+
+    if (is.na(lopref)) stop("Could not find numeric entries in matrix\n")
+
+    # In case user did not remove degeneracies from the input similarities,
+    # avoid degenerate solutions by adding a small amount of noise to the
+    # input similarities; we do this here before running apcluster() in order
+    # to have deterministic behavior during bisection
+    if (!nonoise)
+    {
+        randomMat <- matrix(rnorm(N*N),N)
+
+        s <- s + (.Machine$double.eps * s + .Machine$double.xmin * 100) *
+                 randomMat
+    }
+    
+    # try to guess better lower bound  before starting with bisection
+    ex <- -3
+    dn <- FALSE
+
+    while (!dn)
+    {
+        tmppref <- hipref - 10^ex * (hipref - lopref)
+
+        cat("Trying p =", tmppref, "\n")
+
+        apresultObj <- apcluster(s, p=tmppref, nonoise=TRUE, ...)
+
+        tmpk <- length(apresultObj@exemplars)
+
+        cat("   Number of clusters:", tmpk, "\n");
+        
+        if (tmpk < K)
+        {
+            lok <- tmpk
+            lopref <- tmppref
+            dn <- TRUE
+        }
+        else if (ex == -1)
+        {
+            dn <- TRUE
+        }
+        else
+        {
+            ex <- ex + 1
+        }
+    }
+
+    # now do bisection (if still necessary)
+
+    ntries <- 0
+    
+    while ((abs(tmpk - K) * 100 / K) > prc && ntries < bimaxit)
+    {
+        ntries <- ntries + 1
+        
+        tmppref <- (lopref + hipref) / 2
+        
+        cat("Trying p = ", tmppref, " (bisection step no. ", ntries, ")\n",
+            sep="")
+
+        apresultObj <- apcluster(s, p=tmppref, nonoise=TRUE, ...)
+
+        tmpk <- length(apresultObj@exemplars)
+
+        cat("   Number of clusters:", tmpk, "\n");
+
+        if (K > tmpk)
+        {
+            lopref <- tmppref
+            lok    <- tmpk
+        }
+        else
+        {
+            hipref <- tmppref
+            hik    <- tmpk
+        }
+    }
+
+    cat("\nNumber of clusters:", tmpk, " for p = ", tmppref, "\n")
+
+    apresultObj
+}
---
+++ R/similarity.R
@@ -0,0 +1,64 @@
+negDistMat <- function(x, r=1, ...)
+{
+    dm <- as.matrix(dist(x, diag=TRUE, upper=TRUE, ...))
+
+    if (r <= 0)
+    {
+         warning("ignoring r<=0; using default r=1")
+         r <- 1
+    }
+
+    if (r != 1)
+    {
+        dm <- apply(dm, c(1,2), function(x){x^r})
+    }
+
+    -dm
+}
+
+expSimMat <- function(x, r=2, w=1, ...)
+{
+    dm <- as.matrix(dist(x, diag=TRUE, upper=TRUE, ...))
+
+    if (r <= 0)
+    {
+        warning("ignoring r<=0; using default r=1")
+        r <- 1
+    }
+
+    if (w <= 0)
+    {
+        warning("ignoring w<=0; using default w=1")
+        w <- 1
+    }
+
+    apply(dm, c(1,2), function(x){exp(-(x / w)^r)})
+}
+
+linSimMat <- function(x, w=1, ...)
+{
+    dm <- as.matrix(dist(x, diag=TRUE, upper=TRUE, ...))
+
+    if (w <= 0)
+    {
+        warning("ignoring w<=0; using default w=1")
+        w <- 1
+    }
+
+    apply(dm, c(1,2), function(x){max(0, 1 - x / w)})
+}
+
+linKernel <- function(x, normalize=FALSE)
+{
+    mat <- x %*% t(x)
+
+    if (normalize)
+    {
+        di <- 1 / sqrt(diag(mat))
+        di[which(is.infinite(di))] <- 0
+
+        mat <- mat * (di %o% di)
+    }
+
+    mat
+}
